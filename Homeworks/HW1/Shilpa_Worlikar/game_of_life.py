3dd74cdb9134ece89b97fe92934d67157ea35cd8
shilpaaw <>
2021-12-13 11:14:45 +0530

    Updating python code

--- Homeworks/HW1/Shilpa_Worlikar/game_of_life.py
+++ Homeworks/HW1/Shilpa_Worlikar/game_of_life.py
@@ -1,6 +1,23 @@
 def evolve(initial_state):
     # fill this out
-    pass
+    rows = len(initial_state) 
+	columns = len(initial_state[0])
+
+	matrix = [[0 for j in range(columns)] for i in range(rows)]
+    for i in range(rows):
+        for j in range(columns):
+            if initial_state[i][j] == 1:
+                for k in [[0,-1],[0,1],[-1,-1],[1,1],[-1,0],[1,0],[-1,1],[1,-1]]:
+                    if i+k[0] >= 0 and i+k[0] < len(initial_state) and j+k[1] >= 0 and j+k[1] < len(initial_state[0]):
+                        matrix[i+k[0]][j+k[1]] += 1
+
+    for i in range(rows):
+        for j in range(columns):
+            if initial_state[i][j] == 1 and (matrix[i][j] != 2 and matrix[i][j] != 3):
+                initial_state[i][j] = 0
+            if initial_state[i][j] == 0 and matrix[i][j] == 3:
+                initial_state[i][j] = 1
+    return initial_state
 
 
 test_case_1 = [
