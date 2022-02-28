bf5c4b534579ba53f2734038ebb63883f56a5ef0
buttermilkk2 <95685296+buttermilkk2@users.noreply.github.com>
2022-03-01 02:05:41 +0900

    Add files via upload

---
+++ Homeworks/HW9/endpoints.py
@@ -0,0 +1,39 @@
+import pickle
+
+from flask import Flask
+from model_training import preping_data
+
+
+def creat_app():
+    app = Flask(__name__)
+    app.model = pickle.load(open('Downloads/hw9/data/trained_model.pkl', 'rb'))
+    # this is not the best way to do it
+    # app.data = X
+    app.data, _ = preping_data('sqlite:////Downloads/hw9/data/data.db')
+    return app
+
+
+app = creat_app()
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/health_check")
+def ping():
+    return "pong"
+
+
+@app.route("/<string:time_stamp>")
+def predict_rel_sol(time_stamp: str):
+    # time stamp should be of the form: "2021-11-01 00:00:00"
+    ret_sol = app.model.predict(
+        app.data.loc[time_stamp].to_frame().T
+    )[0]
+    return {"ret_sol": ret_sol}
+
+
+if __name__ == '__main__':
+    app.run("localhost", port=5000)
